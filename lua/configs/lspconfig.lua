ffcfcae930c49300d13e9a8a1f75c7b4693c4b02
thaiph99 <thaiph99@gmail.com>
2025-03-19 01:45:27 +0700

    Update: Change root_dir pyright

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -11,5 +11,9 @@ for _, lsp in ipairs(servers) do
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
   }
+
+  if lsp == "pyright" then
+    config.root_dir = lspconfig.util.root_pattern(".git", "requirements.txt")
+  end
   lspconfig[lsp].setup(config)
 end
