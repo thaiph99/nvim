c6bae44244bb780e5f4b1086eff1a2a88ca1f79a
thaiph99 <thaiph99@gmail.com>
2025-03-23 13:14:21 +0700

    Feat: Use telescope instead of lsp to find references

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -5,9 +5,20 @@ local lspconfig = require "lspconfig"
 local servers = { "html", "cssls", "pyright", "clangd", "gopls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
+-- Change lsp.buf.references to telescope.builtin.lsp_references
+local remapping = function(client, bufnr)
+  nvlsp.on_attach(client, bufnr)
+  vim.keymap.set(
+    "n",
+    "gr",
+    "<cmd>Telescope lsp_references<cr>",
+    { buffer = bufnr, desc = "Telescope lsp go to references" }
+  )
+end
+
 for _, lsp in ipairs(servers) do
   local config = {
-    on_attach = nvlsp.on_attach,
+    on_attach = remapping,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
   }
