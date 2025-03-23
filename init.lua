545aa8d83c0277d6a8ea648162b98f3b3c276b61
thaiph99 <thaiph99@gmail.com>
2025-03-23 13:13:09 +0700

    Feat: Autosave with Cursor hold in normal mode

--- init.lua
+++ init.lua
@@ -14,11 +14,24 @@ vim.opt.wrap = false
 vim.opt.foldmethod = "indent"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 99
+
 vim.api.nvim_create_autocmd("BufEnter", {
   pattern = "*",
   callback = function()
     vim.opt.formatoptions:remove "o"
     vim.opt.formatoptions:remove "r"
+  end,
+})
+
+-- autosave
+vim.o.autowriteall = true
+vim.o.updatetime = 2000 -- Set idle timeout to 2 seconds
+vim.api.nvim_create_autocmd("CursorHold", {
+  pattern = "*",
+  callback = function()
+    if vim.bo.modified then
+      vim.cmd "silent! wa"
+    end
   end,
 })
 
