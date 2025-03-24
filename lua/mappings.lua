5cc48a0dc3481e5b44f1c6746c006beaaa724dd4
thaiph99 <thaiph99@gmail.com>
2025-03-24 21:01:55 +0700

    Feat: live grep with select word

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -11,3 +11,38 @@ map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 map("i", "<C-l>", function()
   vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
 end, { desc = "Copilot Accept", noremap = true, silent = true })
+
+-- telescope live_grep with select word
+local telescope = require "telescope.builtin"
+map("v", "<leader>fw", function()
+  local v_start = vim.fn.getpos "v"
+  local v_end = vim.fn.getpos "."
+
+  local ls, cs = v_start[2], v_start[3]
+  local le, ce = v_end[2], v_end[3]
+
+  if ls > le or (ls == le and cs > ce) then
+    ls, le = le, ls
+    cs, ce = ce, cs
+  end
+
+  local lines = vim.fn.getline(ls, le)
+  if type(lines) == "string" then
+    lines = { lines }
+  end
+
+  if #lines == 0 then
+    return
+  end
+  lines[1] = lines[1]:sub(cs, -1)
+  if #lines == 1 then
+    lines[1] = lines[1]:sub(1, ce - cs + 1)
+  else
+    lines[#lines] = lines[#lines]:sub(1, ce)
+  end
+
+  local text = table.concat(lines, " ")
+  if text ~= "" then
+    telescope.live_grep { default_text = text }
+  end
+end, { desc = "Live grep selected word", silent = true })
