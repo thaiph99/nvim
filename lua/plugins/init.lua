a3e20d4a5bbcdfc9d3b1810352da4aec0092bccf
thaiph99 <thaiph99@gmail.com>
2025-03-28 01:23:38 +0700

    Add: CopilotChat.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -64,6 +64,21 @@ return {
       vim.cmd "hi IlluminatedWordWrite guibg=#525252"
     end,
   },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      window = {
+        width = 0.4,
+      },
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 
   -- {
   --   "hrsh7th/nvim-cmp",
