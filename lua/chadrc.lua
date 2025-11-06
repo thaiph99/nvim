f2b544b04cb801bcddad81992a7dcdd8b435d516
thaiph99 <thaiph99@gmail.com>
2025-11-07 00:00:20 +0700

    feat: add custom statusline

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -21,6 +21,35 @@ M.base46 = {
 --      }
 --}
 
+vim.cmd "highlight St_relativepath guifg=#bbbbbb guibg=#2a2b36"
+
+local stbufnr = function()
+  return vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)
+end
+
+M.ui = {
+  statusline = {
+    theme = "vscode_colored",
+    order = { "mode", "relativepath", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+    hl_add = {
+      St_relativepath = {
+        bg = "black2",
+      },
+    },
+    modules = {
+      relativepath = function()
+        local path = vim.api.nvim_buf_get_name(stbufnr())
+
+        if path == "" then
+          return ""
+        end
+
+        return "%#St_relativepath# " .. vim.fn.expand "%:.:h" .. "/"
+      end,
+    },
+  },
+}
+
 M.term = {
   float = {
     row = 0.1,
