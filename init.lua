4808717ef82b0f986f6edbf6ff2c1cd2f9e0977a
thaiph99 <thaiph99@gmail.com>
2026-02-12 00:58:32 +0700

    chore

--- init.lua
+++ init.lua
@@ -10,32 +10,6 @@ if not vim.uv.fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
-vim.opt.wrap = false
-vim.opt.foldmethod = "indent"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldlevel = 99
-vim.o.title = true
-vim.o.titlestring = "nvim - " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*",
-  callback = function()
-    vim.opt.formatoptions:remove "o"
-    vim.opt.formatoptions:remove "r"
-  end,
-})
-
--- autosave
-vim.o.autowriteall = true
-vim.o.updatetime = 2000 -- Set idle timeout to 2 seconds
-vim.api.nvim_create_autocmd("CursorHold", {
-  pattern = "*",
-  callback = function()
-    if vim.bo.modified then
-      vim.cmd "silent! wa"
-    end
-  end,
-})
 
 local lazy_config = require "configs.lazy"
 
--- lua/options.lua
+++ lua/options.lua
@@ -7,3 +7,30 @@ o.cursorlineopt = "both" -- to enable cursorline!
 
 -- Prevent cursor from being at the first or last line
 vim.opt.scrolloff = 5
+vim.opt.wrap = false
+vim.opt.foldmethod = "indent"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 99
+vim.o.title = true
+vim.o.titlestring = "nvim - " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  --
+  pattern = "*",
+  callback = function()
+    vim.opt.formatoptions:remove "o"
+    vim.opt.formatoptions:remove "r"
+  end,
+})
+
+-- autosave
+vim.o.autowriteall = true
+vim.o.updatetime = 2000 -- Set idle timeout to 2 seconds
+vim.api.nvim_create_autocmd("CursorHold", {
+  pattern = "*",
+  callback = function()
+    if vim.bo.modified then
+      vim.cmd "silent! update"
+    end
+  end,
+})
