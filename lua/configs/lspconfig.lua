4be5dff3ed366c2ee5b1f7721f9ba54aacb11610
thaiph99 <thaiph99@gmail.com>
2026-03-31 00:42:39 +0700

    chore: update clang lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -7,18 +7,7 @@ local servers = {
     root_markers = { "requirements.txt", ".git" },
   },
   clangd = {
-    cmd_env = {
-      CPLUS_INCLUDE_PATH = table.concat({
-        "/usr/include/c++/11",
-        "/usr/include/x86_64-linux-gnu/c++/11",
-        "/usr/include/c++/11/backward",
-        "/usr/lib/gcc/x86_64-linux-gnu/11/include",
-        "/usr/local/include",
-        "/usr/lib/gcc/x86_64-linux-gnu/11/include-fixed",
-        "/usr/include/x86_64-linux-gnu",
-        "/usr/include",
-      }, ":"),
-    },
+    cmd = { "clangd", "--background-index", "--clang-tidy" },
     root_marker = { "." },
   },
   gopls = {},
